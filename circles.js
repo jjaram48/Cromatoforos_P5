ce583df9cb86b208a2c72caf6b695e1b4ce2c11d
Julian Jaramillo <jjaram48@gmail.com>
2016-09-08 19:28:59 +0200

    added separation, max and min r, and unified variables

--- circles.js
+++ circles.js
@@ -1,16 +1,18 @@
-function calc_circles () {
+function calc_circles (blob_r_min_t1, blob_r_min_t2) {
    
-    // var overlapping = false;
-    var protection = 0;
+  var blob_r_min_in = blob_r_min_t1;
+  var blob_r_max_in = blob_r_min_t2;
+  // var overlapping = false;
+  var protection = 0;
   
   // blob = new Blob(0,0,200);
   
-while(circles.length < 255){
+while(circles.length < blobs_number){
   // for (var i = 0; i < 25; i++) {
     var circle = {
       x: random(width),
       y: random(height),
-      r: random(10, 25)
+      r: random(blob_r_min_in,blob_r_max_in)
     }
 
   var overlapping = false;
@@ -19,8 +21,8 @@ while(circles.length < 255){
     var other = circles[j];
     var d = dist(circle.x, circle.y, other.x, other.y);
 
-    if (d < circle.r + other.r) {
-      //THEY ARE OVERLAPPING!
+    if (d < circle.r + other.r + separation) {
+      //THEY ARE OVERLAPPING OR EITHER TOO CLOSE!!!
       overlapping = true;
       break;
     }
@@ -33,8 +35,12 @@ while(circles.length < 255){
   protection++;
   
   if (protection > 10000){
-    println("could not fit them all" );
+    println("could not fit them all, only " + circles.length);
     break;
+  }
+  
+  if (circles.length >= blobs_number) {
+      println("They're all in!");
   }
   
   // println(protection);
--- sketch.js
+++ sketch.js
@@ -1,14 +1,15 @@
 var blob;
 var blobs = [];
-var blobs_number = 15;
-var zoom = 1;
+var blobs_number = 255;
 var circles = [];
-var blob_r = 50;
+var blob_r_min = 10;
+var blob_r_max = 25;
+var separation = 1;    // The real separation is half this value
 
 function setup() {
   createCanvas(600, 600);
  
-  calc_circles();
+  calc_circles(blob_r_min, blob_r_max);
   
   // for (var i = 0; i < blobs_number; i++) {
   //   blobs[i] = new Blob(random(width), random(height), blob_r);
