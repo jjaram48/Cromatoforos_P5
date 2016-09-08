5c301d4b43eb2ef0151186c8a5076316fd32625a
Julian Jaramillo <jjaram48@gmail.com>
2016-09-08 19:52:21 +0200

    migrated creation of shapes from draw to circles

--- circles.js
+++ circles.js
@@ -47,3 +47,33 @@ function calc_circles(blob_r_min_t1, blob_r_min_t2) {
 
   }
 }
+
+function display(){
+
+    for (var i = 0; i < circles.length; i++) {
+    // THESE ARE PLAIN ELLIPSES
+    // fill(255, 0, 150, 100);
+    // noStroke();
+    // ellipse(circles[i].x, circles[i].y, circles[i].r * 2, circles[i].r * 2);
+    
+    
+    // THESE ARE SHAPES
+    
+    fill(255, 0, 150, 100);
+    noStroke();
+    
+    push();
+    translate(circles[i].x, circles[i].y);
+    beginShape();
+
+    for (var a = 0; a < TWO_PI; a += 0.1) {
+      var r_ = circles[i].r;
+      var x_ = circles[i].r * cos(a);
+      var y_ = circles[i].r * sin(a);
+      vertex(x_, y_);
+
+    }
+    endShape();
+    pop();
+  }
+}
--- sketch.js
+++ sketch.js
@@ -20,33 +20,7 @@ function setup() {
 function draw() {
   background(255);
   
-    for (var i = 0; i < circles.length; i++) {
-    // THESE ARE PLAIN ELLIPSES
-    // fill(255, 0, 150, 100);
-    // noStroke();
-    // ellipse(circles[i].x, circles[i].y, circles[i].r * 2, circles[i].r * 2);
-    
-    
-    // THESE ARE SHAPES
-    
-    fill(255, 0, 150, 100);
-    noStroke();
-    
-    push();
-    translate(circles[i].x, circles[i].y);
-    beginShape();
-
-    for (var a = 0; a < TWO_PI; a += 0.1) {
-      var r_ = circles[i].r;
-      var x_ = circles[i].r * cos(a);
-      var y_ = circles[i].r * sin(a);
-      vertex(x_, y_);
-
-    }
-    endShape();
-    pop();
- 
-  }
+  display();
 
   // for (var i = 0; i < blobs.length; i++) {
   
