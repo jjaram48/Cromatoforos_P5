b05514d6782a8e83386f3efba648cccd9b19a268
Julian Jaramillo <jjaram48@gmail.com>
2016-09-08 18:29:36 +0200

    moved circles into circles.js file

---
+++ circles.js
@@ -0,0 +1,49 @@
+function calc_circles () {
+   
+    // var overlapping = false;
+    var protection = 0;
+  
+  // blob = new Blob(0,0,200);
+  
+while(circles.length < 255){
+  // for (var i = 0; i < 25; i++) {
+    var circle = {
+      x: random(width),
+      y: random(height),
+      r: random(10, 25)
+    }
+
+  var overlapping = false;
+  
+  for (var j = 0; j < circles.length; j++) {
+    var other = circles[j];
+    var d = dist(circle.x, circle.y, other.x, other.y);
+
+    if (d < circle.r + other.r) {
+      //THEY ARE OVERLAPPING!
+      overlapping = true;
+      break;
+    }
+  }
+
+  if (!overlapping) {
+    circles.push(circle);
+  }
+  
+  protection++;
+  
+  if (protection > 10000){
+    println("could not fit them all" );
+    break;
+  }
+  
+  // println(protection);
+  
+}
+
+  for (var i = 0; i < circles.length; i++) {
+    fill(255, 0, 150, 100);
+    noStroke();
+    ellipse(circles[i].x, circles[i].y, circles[i].r * 2, circles[i].r * 2);
+  }
+}
--- sketch.js
+++ sketch.js
@@ -6,53 +6,8 @@ var circles = [];
 
 function setup() {
   createCanvas(600, 600);
-  
-    // var overlapping = false;
-    var protection = 0;
-  
-  // blob = new Blob(0,0,200);
-  
-while(circles.length < 255){
-  // for (var i = 0; i < 25; i++) {
-    var circle = {
-      x: random(width),
-      y: random(height),
-      r: 25
-    }
-
-  var overlapping = false;
-  
-  for (var j = 0; j < circles.length; j++) {
-    var other = circles[j];
-    var d = dist(circle.x, circle.y, other.x, other.y);
-
-    if (d < circle.r + other.r) {
-      //THEY ARE OVERLAPPING!
-      overlapping = true;
-      break;
-    }
-  }
-
-  if (!overlapping) {
-    circles.push(circle);
-  }
-  
-  protection++;
-  
-  if (protection > 10000){
-    println("could not fit them all" );
-    break;
-  }
-  
-  // println(protection);
-  
-}
-
-  for (var i = 0; i < circles.length; i++) {
-    fill(255, 0, 150, 100);
-    noStroke();
-    ellipse(circles[i].x, circles[i].y, circles[i].r * 2, circles[i].r * 2);
-  }
+ 
+  calc_circles();
   // for (var i = 0; i < blobs_number; i++) {
   //   blobs[i] = new Blob(random(width), random(height), 50);
   // }
